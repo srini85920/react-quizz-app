f1d1df647fcf9caf0ab7ffe62d2e9f8261e83c1f
Srinivas S <59562657+srini85920@users.noreply.github.com>
2025-12-02 20:54:15 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-
 const Loader = ({ message = "Loading..." }) => (
   <div className="loader-container">
     <div className="loader"></div>
     <p>{message}</p>
   </div>
 );
-
-// This is the crucial line that was likely missing
 export default Loader;
