0b28c2e6d8dee6da749293d68909991bdeaa8fb5
Srinivas S <59562657+srini85920@users.noreply.github.com>
2025-12-02 20:58:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
-// src/main.jsx
-
-import React from 'react'; // --- ADD THIS LINE ---
+import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-
 import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
       <App />
- 
 );
